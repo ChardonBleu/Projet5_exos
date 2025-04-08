b0eb97bbcb25807985a0a9b8e7a33d709143f6b9
ChardonBleu <f.rondi@orange.fr>
2025-04-08 10:35:13 +0200

    test results forlder

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,15 @@
 //votre code ici
 
-export default printNumbers
+function printNumbers(n) {
+    let result = '1'
+    for (let index = 2 ;index < Number(n) + 1; index++) {
+        result += " " + index
+    }
+    // const body = document.querySelector('body')
+    // const numbers = document.createElement('p')
+    // numbers.textContent = result
+    // body.appendChild(numbers)
+    return result
+}
+
+export default printNumbers;
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,7 @@
 //Votre code ici
+function add(nombreEntier, nombreRelatif) {
+    return Number(nombreEntier) + Number(nombreRelatif)
+}
+
 
  export default add;
